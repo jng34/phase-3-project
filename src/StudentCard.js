5ce4608c11e8009a8d361cfa75faab417f4222b3
JamesNg <njames15@gmail.com>
2022-06-20 18:15:47 -0400

    rendered ced student cards

--- src/StudentCard.js
+++ src/StudentCard.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-function StudentCard({ students }) {
-    const { id, name, profile_img, feeling } = students
-
+function StudentCard({ id, name, profile_img, feeling }) {
     return (
        <div>
-            <p>id: {id}</p>
+            <p>id:{id}</p>
             <p>{name}</p>
-            <p>feeling: {feeling}</p>
-            <img src={profile_img} />
+            <p>feeling:{feeling}</p>
+            <img src={profile_img} alt='student'/>
        </div>
     )
 }
--- src/Students.js
+++ src/Students.js
@@ -3,20 +3,20 @@ import StudentCard from './StudentCard';
 
 function Students() {
     // const [students, setStudents] = useState([])
-    const [test, setTest] = useState([])
+    // const [test, setTest] = useState([])
     const [students, setStudents] = useState([])
 
-    useEffect(() => {
-        fetch('http://localhost:9292/')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setTest(data);
-        })
-    }, [])
+    // useEffect(() => {
+    //     fetch('http://localhost:9292')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setTest(data);
+    //     })
+    // }, [])
 
     useEffect(() => {
-        fetch('http://localhost:9292/students/1')
+        fetch('http://localhost:9292/students')
         .then(res => res.json())
         .then(data => {
             console.log(data);
@@ -24,15 +24,16 @@ function Students() {
         })
     }, [])
     
-    
+    const renderStudents = students.map((student) => (
+        <StudentCard key={student.id} id={student.id} name={student.name} profile_img={student.profile_img} feeling={student.feeling}/>
+    ))
+
 
     return (
         <div>
             <h3>Student cards here</h3>
-            <p>TEST: {test.message}</p>
-            <StudentCard students={students} />
+            {renderStudents}
         </div>
-
     )
 }
 

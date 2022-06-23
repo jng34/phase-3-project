9d0a5b1c2778c82dcf977b9ee6387d042ed9ee61
JamesNg <njames15@gmail.com>
2022-06-23 12:29:27 -0400

    fixed usercard display

--- src/UserCard.js
+++ src/UserCard.js
@@ -3,69 +3,26 @@ import React from 'react';
 function UserCard({ user_id, name, image, emoji, drink, handleDelete }) {
 
     return (
-
-        <div>
-            <div key={user_id} id="flip-card" className="col">
-                <div id="flip-card-inner" className='card h-100'>
-                    <div id="flip-card-front">
-                        <img src={image} alt="profile" className='card-img-top' />
-                    </div>
-                    <div id="flip-card-back">
-                        <h5>{name}</h5>
-                        <label>Emoji</label>&nbsp;
-                        <img id='emoji' src={emoji.icon} alt='img' /><br /><br />
-                        <label>Drink</label>&nbsp;&nbsp;
-                        <img id='drink' src={drink.image} alt='img' />
-                        <div className="card-footer" >
-                            <button className="fw-semibold" onClick={() => alert('Smelly~~')}>Edit</button>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <button className="font-monospace fw-semibold bg-danger" onClick={() => handleDelete(user_id)}>$ git out</button>
-                        </div>
+        <div key={user_id} id="flip-card" className="col">
+            <div id="flip-card-inner" className='card h-100'>
+                <div id="flip-card-front">
+                    <img src={image} alt="profile" className='card-img-top' />
+                    <p><strong>{name}</strong></p>
+                </div>
+                <div id="flip-card-back">
+                    <h5>{name}</h5>
+                    <label>Emoji</label>&nbsp;
+                    <img id='emoji' src={emoji.icon} alt='img' /><br /><br />
+                    <label>Drink</label>&nbsp;&nbsp;
+                    <img id='drink' src={drink.image} alt='img' />
+                    <div className="card-footer" >
+                        <button className="fw-semibold" onClick={() => alert('Smelly~~')}>Edit</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <button className="font-monospace fw-semibold bg-danger" onClick={() => handleDelete(user_id)}>$ git out</button>
                     </div>
                 </div>
             </div>
-
-
-{/* <div class="row row-cols-1 row-cols-md-3 g-4">
-<div class="col">
-  <div class="card h-100">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-<div class="col">
-  <div class="card h-100">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-<div class="col">
-  <div class="card h-100">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-</div> */}
-
-    </div>
+        </div>
     )
 }
 
